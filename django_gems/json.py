9fa8de5f27255b4b186172e535331a81cd7c5e8c
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2011-09-13 17:58:28 +0200

    Added JSONResponseMixin.

--- django_gems/json.py
+++ django_gems/json.py
@@ -35,8 +35,6 @@
 # Adapted from
 #		http://code.google.com/p/dojango/source/browse/trunk/dojango/util/__init__.py
 #
-# $Id: json.py 76 2011-08-19 16:02:51Z fiorini $
-#
 
 import os, datetime
 from decimal import Decimal
@@ -153,3 +151,23 @@ def json_view(func):
 		response["If-Modified-Since"] = str(datetime.datetime.now())
 		return response
 	return _wrap
+
+class JSONResponseMixin(object):
+	def render_to_response(self, context):
+		"Returns a JSON response containing 'context' as payload."
+		return self.get_json_response(self.convert_context_to_json(context))
+	
+	def get_json_response(self, content, **httpresponse_kwargs):
+		"Construct a `HttpResponse` object."
+		import datetime
+		response = HttpResponse(content,
+				mimetype="application/json",
+				**httpresponse_kwargs)
+		response["Pragma"] = "no-cache"
+		response["Cache-Control"] = "must-revalidate"
+		response["If-Modified-Since"] = str(datetime.datetime.now())
+		return response
+	
+	def convert_context_to_json(self, context):
+		"Convert the context dictionary into a JSON object."
+		return json_encode(context)
