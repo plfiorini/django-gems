42d274277de9df20332373c9114aef41c34cd806
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2011-12-01 17:01:04 +0100

    Bump version to 0.2.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name="django-gems",
-	version="0.2.5",
+	version="0.2.6",
 	description="Django utilities library",
 	long_description="""Contents of this library:
 		* jqgrid integration with Django
